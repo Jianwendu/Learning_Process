aed5cfb48a2406ede37ec8fac37201b7a7c35fcd
Jianwendu <73426675+Jianwendu@users.noreply.github.com>
2022-04-12 20:12:13 -0400

    project3

---
+++ Lecture_Note/Lecture11-note/json.js
@@ -0,0 +1,24 @@
+// filter
+const filterbn = document.querySelector("button")
+
+function filterfn(event) {
+    console.log("clicked",event.target)
+}
+
+
+filterbn.addEventListener('click',filterfn)
+
+
+
+
+
+
+
+
+// sorting
+
+
+
+
+
+// searching
