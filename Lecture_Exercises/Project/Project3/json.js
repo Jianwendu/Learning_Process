6b1666b92d1eca48d1c3d0e282548f0007dbfe0a
Jianwendu <73426675+Jianwendu@users.noreply.github.com>
2022-04-15 18:33:44 -0400

    sort & search

--- Lecture_Exercises/Project/Project3/json.js
+++ Lecture_Exercises/Project/Project3/json.js
@@ -1,6 +1,7 @@
 // get data
-const gallery = document.querySelector(".container").querySelectorAll('a')
-
+const gallery = document.querySelector(".container").querySelectorAll('a');
+const galleryarray = Array.from(gallery);
+const container = document.querySelector(".container");
 
 // filter
 const filterbn = document.querySelector(".category");
@@ -23,9 +24,46 @@ filterbn.addEventListener('click',filterfn);
 
 
 // sorting
+const sortbn = document.querySelector(".sortbox")
 
+function render(event) {
+    for (i=0;i<gallery.length;i++) {
+        container.appendChild(event[i]);
+    }
+}
 
+function sortfn(event) {
+    const sortcategory = event.target.getAttribute('data-sort');
+    if (sortcategory === 'up') {
+        // clean & render
+        container.innerHTML = "";
+        render(gallery);
+    }
+    else if (sortcategory === 'down') {
+        // clean & reverse
+        container.innerHTML = "";
+        render(galleryarray.reverse())
+    }
+}
 
-
+sortbn.addEventListener('click',sortfn);
 
 // searching
+const searchbar = document.querySelector("input");
+
+function search(event) {
+    const searchcontent = event.target.value;
+    console.log(searchcontent);
+    const name = document.querySelector(".container").getElementsByTagName('p');
+    for (i=0;i<gallery.length;i++) {
+        if (name[i].textContent.includes(searchcontent)) {
+            gallery[i].classList.remove('none');
+        }
+        else {
+            gallery[i].classList.add("none");
+        }
+    }
+}
+
+searchbar.addEventListener("keyup", search);
+
